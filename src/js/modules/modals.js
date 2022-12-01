3c4d88037c0ee702216eec790b8c9f39d44ca17b
Ivan <ivan@mail.ru>
2022-12-01 18:36:20 +0300

    modal start

---
+++ src/js/modules/modals.js
@@ -0,0 +1,76 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector);
+
+        const windows = document.querySelectorAll('[data-modal]');
+        const scroll = calcScroll();
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                windows.forEach(item => {
+                    item.style.display = '';
+                });
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
+            
+            });
+        });
+        
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = '';
+            });
+
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = '';
+                });
+
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;    
+            }
+        });
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    //showModalByTime('.popup_call', 5000);
+};
+
+export default modals;
