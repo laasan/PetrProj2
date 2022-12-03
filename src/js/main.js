5f3687da577b55b22df022252f8cbf28ca002591
Ivan <ivan@mail.ru>
2022-12-03 12:56:16 +0300

    forms and servers

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -7,4 +8,5 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.main-slider-item', 'vertical');
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+    forms();
 });
