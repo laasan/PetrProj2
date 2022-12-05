a32a00d7abec2e660d37eeabb326f5e149ef32d4
Ivan <ivan@mail.ru>
2022-12-05 14:27:15 +0300

    styles from db.json

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -3,29 +3,15 @@ import { getResource } from "../services/requests";
 const showMoreStyles = (trigger, wrapper) => {
     const btn = document.querySelector(trigger);
 
-    // cards.forEach(card => {
-    //     card.classList.add('animated', 'fadeInUp');
-    // });
-
-    // btn.addEventListener('click', () => {
-    //     cards.forEach(card => {
-    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
-    //     });
-    //     // btn.style.display = 'none';
-    //     btn.remove();
-    // });
-
     btn.addEventListener('click', function() {        
-        getResource('http://localhost:3000/styles')
-           .then(res => createCards(res))
+        getResource('assets/db.json')
+           .then(res => createCards(res.styles))
            .catch(error => console.log(error));
 
         this.remove();
     });
 
     function createCards(response) {
-        // console.log(response);
         response.forEach(item => {
             let card = document.createElement('div');
             
@@ -42,14 +28,6 @@ const showMoreStyles = (trigger, wrapper) => {
             document.querySelector(wrapper).appendChild(card);
         });
     }
-    
-//     <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
-//     <div class=styles-block>
-//         <img src=assets/img/styles-5.jpg alt>
-//         <h4>Пастелью</h4>
-//         <a href="#">Подробнее</a>
-//     </div>
-// </div>
 };
 
 export default showMoreStyles;
